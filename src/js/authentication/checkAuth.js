140af99b0edea10faf851683dac68f65148757e5
Filip Bergander <filip.bergander1996@gmail.com>
2026-05-05 11:47:20 +0200

    La till lite säkerhet och tömmer formulär inom admin-sidan om besökaren inte har behörighet.

--- src/js/authentication/checkAuth.js
+++ src/js/authentication/checkAuth.js
@@ -2,8 +2,12 @@
 checkAuthAccess();
 // Ser över om användaren har behörighet till admin-sidan
 export function checkAuthAccess() {
+    const adminUser = document.getElementById("admin-user");
+    const newsForm = document.getElementById("add-news-form");
     // Går inte att navigera till admin-sidan om användaren inte är inloggad
     if (window.location.pathname.endsWith("admin.html") && !localStorage.getItem("nyckel")) {
+        adminUser.innerHTML = "";
+        newsForm.innerHTML = "";
         window.location.href = "login.html"; // Redirect till login-sidan
     };
 
